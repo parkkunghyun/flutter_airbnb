c8c1c7fe596948c12eef72e53e6172486f3838f3
baggyeonghyeon <rudgus4620@gmail.com>
2023-11-28 17:25:28 +0900

    header

--- lib/components/common/common_form_field.dart
+++ lib/components/common/common_form_field.dart
@@ -1,10 +1,51 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 class CommonFormField extends StatelessWidget {
 
+  final prefixText;
+  final hintText;
+
+  CommonFormField({required this.prefixText, required this.hintText});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        TextFormField(
+          textAlignVertical: TextAlignVertical.bottom,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.only(top: 30, bottom: 10, left: 20),
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder:  OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.black,
+              width: 2,
+            )
+          ),
+          errorBorder:  OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          // prefix: Text(prefixText), 고정된 텍스트
+         ),
+        ),
+
+        Positioned(
+          top: 8,
+          left: 20,
+          child: Text(prefixText,
+            style: overLine(),
+          ),
+        ),
+      ]
+    );
   }
 }
--- lib/components/home/home_header_form.dart
+++ lib/components/home/home_header_form.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/components/common/common_form_field.dart';
+import 'package:flutter_airbnb/constants.dart';
 import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 class HomeHeaderForm extends StatelessWidget {
 
@@ -9,7 +12,7 @@ class HomeHeaderForm extends StatelessWidget {
       padding: const EdgeInsets.only(top: gap_m),
       child: Align(
         // 2 0.6
-        alignment: Alignment(-0.6, 0),
+        alignment: const Alignment(-0.6, 0),
         child: Container(
           width: 420,
           //height: 100,
@@ -19,12 +22,15 @@ class HomeHeaderForm extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
           child: Form(
-              child: Column(
-                children: [
-                  _buildFormTitle(),
-                  _buildFormField(),
-                  _buildFormSubmit(),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(gap_l),
+                child: Column(
+                  children: [
+                    _buildFormTitle(),
+                    _buildFormField(),
+                    _buildFormSubmit(),
+                  ],
+                ),
               ),
           ),
         ),
@@ -33,13 +39,68 @@ class HomeHeaderForm extends StatelessWidget {
   }
 
   Widget _buildFormTitle() {
-    return SizedBox();
+    return Column(
+      children: [
+        Text("에어비엔비에서 숙소를 검색하세요", style: h4(),),
+        const SizedBox(height: gap_xs,),
+        Text("혼자하는 여행에 적합한 개인실부터 여럿이 함께하는 여행에 좋은 '공간전체' 숙소까지 에어비엔비에 다 있습니다",
+          style: body1(),
+        ),
+        const SizedBox(height: gap_m,),
+      ],
+    );
   }
   Widget _buildFormField() {
-    return SizedBox();
+    return Column(
+      children: [
+        CommonFormField(prefixText: "위치", hintText: "근처 추천 장소"),
+        const SizedBox(height: gap_s,),
+        Row(
+          children: [
+            SizedBox(
+              width: 180,
+                child: CommonFormField(prefixText: "체크인", hintText: "날짜 입력"),
+            ),
+            const SizedBox(width: gap_xs,),
+            Expanded(
+              child: CommonFormField(prefixText: "체크 아웃", hintText: "날짜 입력"),
+            ),
+          ],
+        ),
+
+        const SizedBox(height: gap_s,),
+        Row(
+          children: [
+            SizedBox(
+              width: 180,
+              child: CommonFormField(prefixText: "성인", hintText: "2"),
+            ),
+            const SizedBox(width: gap_xs,),
+            Expanded(
+              child: CommonFormField(prefixText: "어린이", hintText: "0"),
+            ),
+          ],
+        ),
+        const SizedBox(height: gap_m,),
+      ],
+    );
   }
 
   Widget _buildFormSubmit() {
-    return SizedBox();
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: TextButton(onPressed: () {
+        print("submit 클릭됨");
+      },
+        style: TextButton.styleFrom(
+          backgroundColor: kAccentColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+          child: Text("검색", style: subTitle1(mColor: Colors.white),),
+      ),
+    );
   }
 }
